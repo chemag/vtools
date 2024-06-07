vtools: opencv: fix calculation of first delta timestamp
Tested:
Before:
```
$ src/vtools-analysis.py --noframe-dups in.mov -o /tmp/before.csv
...
$ csvcut -c frame_num,delta_timestamp_ms /tmp/before.csv |csvlook -I |head
| frame_num | delta_timestamp_ms |
| --------- | ------------------ |
| 0         | -93.33333333333334 |
| 1         | 126.66666666666669 |
| 2         | 33.333333333333336 |
| 3         | 33.33333333333333  |
| 4         | 33.33333333333334  |
| 5         | 33.33333333333334  |
| 6         | 33.333333333333314 |
| 7         | 38.33333333333334  |
```

After:
```
$ src/vtools-analysis.py --noframe-dups in.mov -o /tmp/after.csv
...
$ csvcut -c frame_num,delta_timestamp_ms /tmp/after.csv |csvlook -I |head
| frame_num | delta_timestamp_ms |
| --------- | ------------------ |
| 0         |                    |
| 1         | 126.66666666666669 |
| 2         | 33.333333333333336 |
| 3         | 33.33333333333333  |
| 4         | 33.33333333333334  |
| 5         | 33.33333333333334  |
| 6         | 33.333333333333314 |
| 7         | 38.33333333333334  |
```

src/vtools-opencv.py
@@ -60,7 +60,7 @@ def run_opencv_analysis(infile, add_mse, mse_delta, debug):
 
     # process the input
     opencv_vals = []
-    prev_timestamp_ms = video_capture.get(cv2.CAP_PROP_POS_MSEC)
+    prev_timestamp_ms = np.nan
     prev_img = None
     frame_num = 0
     while True:
@@ -74,7 +74,10 @@ def run_opencv_analysis(infile, add_mse, mse_delta, debug):
         ]
         # get timestamps
         timestamp_ms = video_capture.get(cv2.CAP_PROP_POS_MSEC)
-        delta_timestamp_ms = timestamp_ms - prev_timestamp_ms
+        if prev_timestamp_ms == np.nan:
+            delta_timestamp_ms = np.nan
+        else:
+            delta_timestamp_ms = timestamp_ms - prev_timestamp_ms
         vals += [timestamp_ms, delta_timestamp_ms]
         if add_mse:
             # get mse